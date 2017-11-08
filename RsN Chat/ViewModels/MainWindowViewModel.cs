6f08afa0f5e459493d598f4ebeab77315dc9eb3e
Urban <urban@rosenation.tv>
2017-11-08 15:54:30 -0500

    added MainWindowViewModel

---
+++ RsN Chat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RsN_Chat.ViewModels
+{
+    class Class1
+    {
+    }
+}
