29df1a4693e2a0cdd96b946f44af083b40b6ffd2
Urban <urban@rosenation.tv>
2017-11-08 16:30:38 -0500

    added viewmodel and menu actions

--- RsN Chat/ViewModels/MainWindowViewModel.cs
+++ RsN Chat/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Threading.Tasks;
 
 namespace RsN_Chat.ViewModels
 {
-    class Class1
+    class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DelegateCommand<string> MenuCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MenuCommand = new DelegateCommand<string>(
+                command => { PerformMenuAction(command); }, // OnExecute
+                command => { return true; } // CanExecute
+            );
+        }
+
+        private void PerformMenuAction(string action)
+        {
+            if (action == "About")
+            {
+                MessageBox.Show("RSN Chat written by Alexander Rose.", "About RsN Chat");
+            }
+        }
     }
 }
